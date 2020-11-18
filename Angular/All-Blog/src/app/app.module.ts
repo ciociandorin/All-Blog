ca759debaa62ebe66667c5e35ed480ea24b1056d
Dorin <dorinciocian@gmail.com>
2020-11-19 00:45:26 +0200

    addAngularInterface (19.11)
    Added Angular Interface

--- Angular/All-Blog/src/app/app.module.ts
+++ Angular/All-Blog/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TopToBottomDirective } from './directives/gsap/top-to-bottom-animation.directive';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -7,11 +8,11 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
 import { PostListComponent } from './post-list/post-list.component';
 import { FadeInAnimationDirective } from './directives/gsap/fade-in-animation.directive';
 import { RightToLeftAnimationDirective } from './directives/gsap/right-to-left-animation.directive';
-import { LoginRegisterBackgroundComponent } from './login-register-background/login-register-background.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { AddPostComponent } from './add-post/add-post.component';
 import { PostComponent } from './post/post.component';
+import { TopToBottomDirective2 } from './directives/gsap/top-to-bottom-animation2.directive';
 
 @NgModule({
   declarations: [
@@ -20,11 +21,12 @@ import { PostComponent } from './post/post.component';
     PostListComponent,
     FadeInAnimationDirective,
     RightToLeftAnimationDirective,
-    LoginRegisterBackgroundComponent,
     LoginFormComponent,
     RegisterFormComponent,
     AddPostComponent,
-    PostComponent
+    PostComponent,
+    TopToBottomDirective,
+    TopToBottomDirective2
   ],
   imports: [
     BrowserModule,
---
+++ Angular/All-Blog/src/app/directives/gsap/top-to-bottom-animation.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, OnInit, OnDestroy, ViewContainerRef, ElementRef, Input, EventEmitter } from '@angular/core';
+import { TimelineMax } from 'gsap';
+import { CoreAnimationDirective } from './core-animation.directive';
+
+@Directive({
+  selector: '[topToBottomAnimation]'
+})
+export class TopToBottomDirective extends CoreAnimationDirective implements OnInit {
+  constructor(protected element: ElementRef) {
+    super(element);
+  }
+
+  ngOnInit() {
+    // perform animation
+    this.animateIn();
+  }
+
+  protected animateIn() {
+    this.timeline.from(this.element.nativeElement, this.duration, {top: "100%", height: "0%", ease:"power4.inOut"}, this.delay);
+    super.animateIn();
+  }
+  
+}
