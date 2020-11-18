3c0b7df40bf947bfb1ef1ae26bc88deebe42c794
Dorin <dorinciocian@gmail.com>
2020-11-18 18:14:10 +0200

    Angular (18.11)
    Home layout

--- Angular/All-Blog/src/app/app.module.ts
+++ Angular/All-Blog/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { NavigationBarComponent } from './navigation-bar/navigation-bar.componen
 import { PostListComponent } from './post-list/post-list.component';
 import { FadeInAnimationDirective } from './directives/gsap/fade-in-animation.directive';
 import { RightToLeftAnimationDirective } from './directives/gsap/right-to-left-animation.directive';
+import { LoginRegisterBackgroundComponent } from './login-register-background/login-register-background.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { RightToLeftAnimationDirective } from './directives/gsap/right-to-left-a
     NavigationBarComponent,
     PostListComponent,
     FadeInAnimationDirective,
-    RightToLeftAnimationDirective
+    RightToLeftAnimationDirective,
+    LoginRegisterBackgroundComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    AddPostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/All-Blog/src/app/login-register-background/login-register-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-register-background',
+  templateUrl: './login-register-background.component.html',
+  styleUrls: ['./login-register-background.component.css']
+})
+export class LoginRegisterBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
