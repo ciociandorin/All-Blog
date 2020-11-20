4bda79f6f4a993658d67cf51ab3441d35cfd08df
Dorin <dorinciocian@gmail.com>
2020-11-20 15:25:19 +0200

    addLoginBackEndAPI

--- Angular/All-Blog/routes/index.router.js
+++ Angular/All-Blog/routes/index.router.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const ctrUser = require('../controllers/user.controller');
 
+const jwtHelper = require('../config/jwtHelper');
+
 router.post('/register', ctrUser.register);
+router.post('/authenticate', ctrUser.authenticate);
+router.get('/userProfile',jwtHelper.verifyJwtToken, ctrUser.userProfile);
 
 module.exports = router;
