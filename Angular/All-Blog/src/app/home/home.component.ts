564dc0c9a5ab36b5692d8198a313eff79b8fed9e
Dorin <dorinciocian@gmail.com>
2020-11-27 23:28:03 +0200

    addHomePostList

--- Angular/All-Blog/src/app/home/home.component.ts
+++ Angular/All-Blog/src/app/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from './../shared/post.model';
+import { PostService } from './../shared/post.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [PostService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public postService: PostService) { }
 
   ngOnInit(): void {
+    this.refreshPostList();
+  }
+
+  refreshPostList() {
+    this.postService.getPostList().subscribe((res) => {
+      this.postService.post = res as Post[];
+    });
   }
 
 }
